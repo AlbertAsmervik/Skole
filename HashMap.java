9706000adf59ff7a635c8db5b14faf1b3b1eb12a
AlbertAsmervik <Albertasmervik@gmail.com>
2023-03-21 22:51:38 +0100

    Practice
    Doing the educative course, and made some tweak to the shapeGenerator.

---
+++ HashMap.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HashMap {
+    public static void main(String[] args) {
+        int number = 5;
+
+        
+        for (int i = 1; i <= number; i++) 
+            {
+            for (int j = 1; j <= i; ++j) {
+                System.out.print("# ");
+                }
+                System.out.println();  
+        }
+    }
+}
